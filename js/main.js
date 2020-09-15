ea25432fccc53a148f15a5fa41ed30c16ef99ef7
TheMirac1e <66904078+TheMirac1e@users.noreply.github.com>
2020-09-15 17:49:31 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        vertical: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    autoplay: false,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    })
+});
